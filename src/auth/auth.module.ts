d021c962efddff23eb7c69fd2b7d21491edf6f39
maxererl <126725331+maxererl@users.noreply.github.com>
2026-05-07 19:36:14 +0300

    Added jwt module with configuration (#27)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtAuthModule } from './jwt/jwt.module';
 
 @Module({
+  imports: [JwtAuthModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
