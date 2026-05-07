64dbb7fc8702fa9fb11621cbc72f9504ec5598aa
maxererl <maxererl@yandex.ru>
2026-05-07 17:32:19 +0300

    fix(config): Fixed name of port env in config

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,7 +1,7 @@
 import { ConfigType, registerAs } from '@nestjs/config';
 
 export const appConfig = registerAs('APP_CONFIG', () => ({
-  port: parseInt(process.env.APP_PORT ?? '3000', 10),
+  port: parseInt(process.env.PORT ?? '3000', 10),
   hashSalt: parseInt(process.env.HASH_SALT ?? '10', 10),
 }));
 
