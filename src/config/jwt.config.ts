75fc5d38b594cd4652ab3b720a1a00192adbfd91
maxererl <126725331+maxererl@users.noreply.github.com>
2026-05-07 20:16:21 +0300

    feat(config): Added jwt config file (#30)
    * feat(config): Added jwt config file

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,10 @@
+import { ConfigType, registerAs } from '@nestjs/config';
+
+export const jwtConfig = registerAs('JWT_CONFIG', () => ({
+  accessSecret: process.env.JWT_ACCESS_SECRET ?? 'your_access_secret_key',
+  accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN ?? '15m',
+  refreshSecret: process.env.JWT_REFRESH_SECRET ?? 'your_refresh_secret_key',
+  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN ?? '7d',
+}));
+
+export type TJwtConfig = ConfigType<typeof jwtConfig>;
