41f4155a493cf71fdd94b8f9ddb9d4d2f2e70f34
Ekaterina Novikova <fearnocait@gmail.com>
2026-05-09 18:26:38 +0300

    feat(users): add updateUserDto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsDateString, IsEmail, IsEnum, IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
+import { Gender } from '../entities/user.entity';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  about: string;
+  @IsDateString()
+  birthdate: string;
+  @IsString()
+  city: string;
+  @IsEnum(Gender)
+  gender: Gender;
+}
