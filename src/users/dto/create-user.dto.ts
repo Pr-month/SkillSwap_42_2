c3f22b278dd80d61bb0f74bab6979c273ac61081
Ekaterina <48985443+albrekhtdurer@users.noreply.github.com>
2026-05-09 19:40:07 +0300

    Описать DTO для юзеров (#39)
    * feat(users): added createUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,32 @@
-export class CreateUserDto {}
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Gender } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  password: string;
+  @IsString()
+  @IsOptional()
+  about: string;
+  @IsDateString()
+  birthdate: string;
+  @IsString()
+  city: string;
+  @IsEnum(Gender)
+  gender: Gender;
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  wantToLearn: string[];
+}
