72ac0fdcf5416ee2fb8d86b89c7096a4b5a3150f
Ekaterina Novikova <fearnocait@gmail.com>
2026-05-09 19:36:12 +0300

    fix(users): use extension for updateUserDto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,19 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsDateString, IsEmail, IsEnum, IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
-import { Gender } from '../entities/user.entity';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsString()
-  name: string;
-  @IsEmail()
-  email: string;
-  @IsString()
-  about: string;
-  @IsDateString()
-  birthdate: string;
-  @IsString()
-  city: string;
-  @IsEnum(Gender)
-  gender: Gender;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
