6b053ac65dc4f9fcaf400c29faf557762fa7578e
Ekaterina Novikova <fearnocait@gmail.com>
2026-05-09 18:20:08 +0300

    feat(users): added createUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserDto {}
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsString,
+} from 'class-validator';
+import { Gender } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  password: string;
+  @IsString()
+  about: string;
+  @IsDateString()
+  birthdate: string;
+  @IsString()
+  city: string;
+  @IsEnum(Gender)
+  gender: Gender;
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  wantToLearn: string[];
+}
