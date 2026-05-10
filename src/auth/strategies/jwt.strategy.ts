7e67b7221ce513c0522350b7fcf8f11bb524ce4d
maxererl <126725331+maxererl@users.noreply.github.com>
2026-05-10 19:50:44 +0300

    Описать стратегию и гарду для защиты эндпоинта по access токену (#44)
    * feat(auth): jwt strategy implemented
    
    * fix(auth): jwt strategy user service use updated
    
    * fix(auth): jwt strategy return value changed to user
    
    * feat(auth): jwt-access guard added

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { jwtConfig, TJwtConfig } from '../../config/jwt.config';
+import { TJwtPayload } from '../auth.types';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    @Inject(jwtConfig.KEY)
+    protected jwtConfig: TJwtConfig,
+    private usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtConfig.accessSecret,
+      ignoreExpiration: false,
+    });
+  }
+
+  async validate(payload: TJwtPayload) {
+    const user = await this.usersService.findOneById(payload.sub);
+    if (!user) throw new UnauthorizedException();
+    return user;
+  }
+}
