aeecde51cde87be3486522bb9cc15add2c571d3b
maxererl <maxererl@yandex.ru>
2026-05-10 17:57:25 +0300

    fix(auth): jwt strategy user service use updated

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConfig, TJwtConfig } from '../../config/jwt.config';
@@ -20,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: TJwtPayload) {
-    const user = await this.usersService.findOne(payload.sub);
+    const user = await this.usersService.findOneById(payload.sub);
     if (!user) throw new UnauthorizedException();
     return { id: payload.sub, email: payload.email, role: payload.role };
   }
