b2d468c7593c12bc69a27f02146ec70b787b211d
maxererl <maxererl@yandex.ru>
2026-05-11 19:45:30 +0300

    fix(auth): auth module imports updated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { UsersModule } from '../users/users.module';
-import { UsersService } from '../users/users.service';
 import { User } from '../users/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { jwtConfig, TJwtConfig } from '../config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     TypeOrmModule.forFeature([User]),
-    UsersModule,
     JwtModule.registerAsync({
       inject: [jwtConfig.KEY],
       useFactory: (jwtConfig: TJwtConfig) => ({
@@ -24,6 +23,6 @@ import { jwtConfig, TJwtConfig } from '../config/jwt.config';
     }),
   ],
   controllers: [AuthController],
-  providers: [UsersService, AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
