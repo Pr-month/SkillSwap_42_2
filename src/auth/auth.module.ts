b6d4ffc51d2ba744b7c7e3189fe55e373c2c50bd
maxererl <maxererl@yandex.ru>
2026-05-11 14:43:20 +0300

    refactor(auth): jwt module merged into auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtAuthModule } from './jwt/jwt.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConfig, TJwtConfig } from '../config/jwt.config';
 
 @Module({
-  imports: [JwtAuthModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    UsersModule,
+    JwtModule.registerAsync({
+      inject: [jwtConfig.KEY],
+      useFactory: (jwtConfig: TJwtConfig) => ({
+        secret: jwtConfig.accessSecret,
+        signOptions: {
+          expiresIn: jwtConfig.accessExpiresIn,
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [UsersService, AuthService, JwtStrategy],
 })
 export class AuthModule {}
