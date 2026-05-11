6e3c5fd0a55443c58a11ae71689a0d4b672c4e9c
maxererl <126725331+maxererl@users.noreply.github.com>
2026-05-11 20:10:31 +0300

    Порефакторить Jwt модуль (#46)
    * refactor(auth): jwt module merged into auth module
    
    * fix(auth): jwt strategy DB user fetch removed
    
    * fix(auth): auth module imports updated
    
    * style(auth): imports order
    
    ---------
    
    Co-authored-by: Ekaterina Novikova <fearnocait@gmail.com>

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtAuthModule } from './jwt/jwt.module';
+import { User } from '../users/entities/user.entity';
+import { jwtConfig, TJwtConfig } from '../config/jwt.config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [JwtAuthModule],
+  imports: [
+    PassportModule,
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      inject: [jwtConfig.KEY],
+      useFactory: (jwtConfig: TJwtConfig) => ({
+        secret: jwtConfig.accessSecret,
+        signOptions: {
+          expiresIn: jwtConfig.accessExpiresIn,
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,16 +1,14 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConfig, TJwtConfig } from '../../config/jwt.config';
 import { TJwtPayload } from '../auth.types';
-import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
     @Inject(jwtConfig.KEY)
     protected jwtConfig: TJwtConfig,
-    private usersService: UsersService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -19,9 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: TJwtPayload) {
-    const user = await this.usersService.findOneById(payload.sub);
-    if (!user) throw new UnauthorizedException();
-    return user;
+  validate(payload: TJwtPayload): TJwtPayload {
+    return { sub: payload.sub, email: payload.email, role: payload.role };
   }
 }
