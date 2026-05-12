0c10b09f74fabeabbc9a0b32543d803a376ee6e4
maxererl <maxererl@yandex.ru>
2026-05-12 22:22:04 +0300

    refactor(users): added method for refresh token and removed mapper on outputs as it will be replaced by decorators later

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { FindUserDto } from './dto/find-user.dto';
 import { User, UserRole } from './entities/user.entity';
 import { appConfig, TAppConfig } from '../config/app.config';
 
@@ -17,17 +16,17 @@ export class UsersService {
     private readonly usersRepository: Repository<User>,
   ) {}
 
-  private findUserMapper = (root: User): FindUserDto => ({
-    id: root.id,
-    name: root.name,
-    email: root.email,
-    about: root.about,
-    birthdate: root.birthdate,
-    city: root.city,
-    gender: root.gender,
-    wantToLearn: root.wantToLearn,
-    createdAt: root.createdAt,
-  });
+  // private findUserMapper = (root: User): FindUserDto => ({
+  //   id: root.id,
+  //   name: root.name,
+  //   email: root.email,
+  //   about: root.about,
+  //   birthdate: root.birthdate,
+  //   city: root.city,
+  //   gender: root.gender,
+  //   wantToLearn: root.wantToLearn,
+  //   createdAt: root.createdAt,
+  // });
 
   private async generateHash(str: string) {
     return await bcrypt.hash(str, this.config.hashSalt);
@@ -44,22 +43,22 @@ export class UsersService {
       role: UserRole.USER,
     });
     const savedUser = await this.usersRepository.save(newUser);
-    return this.findUserMapper(savedUser);
+    return savedUser;
   }
 
   async findAll() {
     const users = await this.usersRepository.find({});
-    return users.map(this.findUserMapper);
+    return users;
   }
 
   async findOneById(id: string) {
     const user = await this.usersRepository.findOneBy({ id });
-    return user ? this.findUserMapper(user) : null;
+    return user;
   }
 
   async findOneByEmail(email: string) {
     const user = await this.usersRepository.findOneBy({ email });
-    return user ? this.findUserMapper(user) : null;
+    return user;
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
@@ -67,7 +66,7 @@ export class UsersService {
     if (!user) return null;
     Object.assign(user, updateUserDto);
     await this.usersRepository.save(user);
-    return this.findUserMapper(user);
+    return user;
   }
 
   async updatePassword(id: string, oldPassword: string, newPassword: string) {
@@ -84,5 +83,11 @@ export class UsersService {
       password: hash,
     });
     return updatedUser;
+  }
+
+  async setRefreshToken(id: string, refreshToken: string | null) {
+    return await this.usersRepository.update(id, {
+      refreshToken,
+    });
   }
 }
