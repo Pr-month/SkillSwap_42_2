f432301223ab40b2c9c02c43ad85da85ad8aa18c
maxererl <maxererl@yandex.ru>
2026-05-12 14:46:09 +0300

    fix(auth): login refresh token returns as cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,18 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Inject, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
+import { Response } from 'express';
+import { jwtConfig, TJwtConfig } from 'src/config/jwt.config';
+import * as ms from 'ms';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConf: TJwtConfig,
+  ) {}
 
   @Post('register')
   register(@Body() dto: CreateUserDto) {
@@ -14,7 +21,22 @@ export class AuthController {
 
   @Post('login')
   @HttpCode(200)
-  login(@Body() dto: LoginDto) {
-    return this.authService.login(dto);
+  async login(
+    @Body() dto: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken, refreshToken } = await this.authService.login(dto);
+    this.setRefreshCookie(res, refreshToken);
+    return { accessToken };
+  }
+
+  private setRefreshCookie(res: Response, token: string) {
+    res.cookie('refresh_token', token, {
+      httpOnly: true,
+      secure: false, // Only for development
+      sameSite: 'strict',
+      maxAge: ms(this.jwtConf.refreshExpiresIn),
+      path: '/auth',
+    });
   }
 }
