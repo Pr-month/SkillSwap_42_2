7d5e84fc96386f4abb0fdb9b137559990c476c00
Ekaterina Novikova <fearnocait@gmail.com>
2026-05-14 13:25:05 +0300

    feat(skills): added skill entity

--- src/skills/entities/skill.entity.ts
+++ src/skills/entities/skill.entity.ts
@@ -1,1 +1,24 @@
-export class Skill {}
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('skills')
+export class Skill {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 150 })
+  title: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  // TODO: Add a relation to skills categories
+  @Column({ type: 'varchar', length: 4, nullable: true })
+  category: string;
+
+  @Column({ type: 'simple-array', nullable: true })
+  images: string[];
+
+  @ManyToOne(() => User, (user) => user.skills)
+  owner: User;
+}
