8d514f8dcb0176bbafb83cfbc3ef8827402d928e
maxererl <maxererl@yandex.ru>
2026-05-16 01:40:11 +0300

    feat(constraints): added constraints messages mapping and chore in filter

--- src/common/all-exception.filter.ts
+++ src/common/all-exception.filter.ts
@@ -6,46 +6,50 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
-import { TypeORMError } from 'typeorm';
-import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
-import { QueryFailedError } from 'typeorm/error/QueryFailedError';
+import { EntityNotFoundError, QueryFailedError } from 'typeorm';
+import {
+  CONSTRAINTS_MESSAGES,
+  DatabaseConstraints,
+} from './database-constraints';
 
 @Catch()
 export class AllExceptionFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    let message: string;
-    let status: HttpStatus;
-    if (exception instanceof TypeORMError) {
-      const result = this.getTypeOrmErrorData(exception);
-      status = result.status;
-      message = result.message;
-    } else {
-      status =
-        (exception as HttpException).getStatus() ||
-        HttpStatus.INTERNAL_SERVER_ERROR;
-      message = (exception as HttpException).message || 'Internal Server Error';
-    }
-    response.status(status).json({
-      statusCode: status,
-      message: message,
-    });
+    const response = host.switchToHttp().getResponse<Response>();
+    const { statusCode, message } = this.getErrorResponse(exception);
+
+    response.status(statusCode).json({ statusCode, message });
   }
 
-  private getTypeOrmErrorData(exception: TypeORMError) {
-    let message: string = exception.message || 'Internal Server Error';
-    let status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+  private getErrorResponse(exception: unknown): {
+    statusCode: HttpStatus;
+    message: string;
+  } {
+    if (exception instanceof HttpException) {
+      return { statusCode: exception.getStatus(), message: exception.message };
+    }
+
     if (exception instanceof EntityNotFoundError) {
-      status = HttpStatus.NOT_FOUND;
-      message = 'Entity not found';
-    } else if (
+      return { statusCode: HttpStatus.NOT_FOUND, message: 'Entity not found' };
+    }
+
+    if (
       exception instanceof QueryFailedError &&
       (exception.driverError as { code: string }).code === '23505'
     ) {
-      status = HttpStatus.CONFLICT;
-      message = 'User already exists';
+      const constraint = (
+        exception.driverError as { constraint: DatabaseConstraints }
+      ).constraint;
+
+      return {
+        statusCode: HttpStatus.CONFLICT,
+        message: CONSTRAINTS_MESSAGES[constraint] ?? 'Entity already exists',
+      };
     }
-    return { status, message };
+
+    return {
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Internal server error',
+    };
   }
 }
