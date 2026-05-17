67148eee3972244989c223becd9fe53ff9e99857
maxererl <maxererl@yandex.ru>
2026-05-17 20:12:22 +0300

    feat(files): added files upload and serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,9 @@ import { databaseConfig, TDatabaseConfig } from './config/database.config';
 import { appConfig } from './config/app.config';
 import { jwtConfig } from './config/jwt.config';
 import { SkillsModule } from './skills/skills.module';
+import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -22,9 +25,16 @@ import { SkillsModule } from './skills/skills.module';
         ...dbConfig,
       }),
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveStaticOptions: {
+        index: false,
+      },
+    }),
     AuthModule,
     UsersModule,
     SkillsModule,
+    FilesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
