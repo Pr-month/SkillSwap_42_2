66b12d46c4de93b31d6e5d4316e710c7ccacd31a
Ekaterina Novikova <fearnocait@gmail.com>
2026-05-19 14:32:08 +0300

    feat(users): add interceptor and exclude fields from response

--- src/users/dto/find-user.dto.ts
+++ src/users/dto/find-user.dto.ts
@@ -1,14 +1,30 @@
-import { Gender } from '../users.enums';
+import { Skill } from 'src/skills/entities/skill.entity';
+import { Gender, UserRole } from '../users.enums';
+import { Exclude } from 'class-transformer';
 
-// TODO: delete if won't be needed
 export class FindUserDto {
   id: string;
   name: string;
   email: string;
+  @Exclude()
+  password: string;
+  role: UserRole;
   about: string;
   birthdate: string;
   city: string;
   gender: Gender;
+  avatar: string;
+  // TODO: replace with GetSkillDto
+  skills!: Skill[];
+  // TODO: replace with GetSkillDto
+  favoriteSkills: Skill[];
+  @Exclude()
+  refreshToken: string | null;
   wantToLearn: string[];
   createdAt: Date;
+  updatedAt: Date;
+
+  constructor(partial: Partial<FindUserDto>) {
+    Object.assign(this, partial);
+  }
 }
