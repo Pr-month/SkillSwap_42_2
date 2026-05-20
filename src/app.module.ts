e3f6eb27df305a52e18154194f4cb833256485d5
DmitryChesnov <advokat33333@gmail.com>
2026-05-20 14:34:50 +0300

    Логирование запросов#65

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-// src/app.module.ts
 import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -10,6 +11,7 @@ import { databaseConfig, TDatabaseConfig } from './config/database.config';
 import { appConfig } from './config/app.config';
 import { jwtConfig } from './config/jwt.config';
 import { SkillsModule } from './skills/skills.module';
+import { FilesModule } from './files/files.module';
 import { LoggerModule } from './logger/logger.module';
 import { LoggerMiddleware } from './logger/logger.middleware';
 
@@ -25,10 +27,17 @@ import { LoggerMiddleware } from './logger/logger.middleware';
         ...dbConfig,
       }),
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveStaticOptions: {
+        index: false,
+      },
+    }),
     LoggerModule,
     AuthModule,
     UsersModule,
     SkillsModule,
+    FilesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
