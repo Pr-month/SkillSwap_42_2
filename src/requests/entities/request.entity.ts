c10fa5750eed6edb1539cddc1f940a204f281835
maxererl <maxererl@yandex.ru>
2026-05-21 13:22:51 +0300

    feat(requests): request entity implemented

--- src/requests/entities/request.entity.ts
+++ src/requests/entities/request.entity.ts
@@ -1,1 +1,44 @@
-export class Request {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Skill } from '../../skills/entities/skill.entity';
+import { RequestStatus } from '../requests.enums';
+
+@Entity('requests')
+export class Request {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @ManyToOne(() => User, { nullable: false, onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'sender_id' })
+  sender!: User;
+
+  @ManyToOne(() => User, { nullable: false, onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'receiver_id' })
+  receiver!: User;
+
+  @Column({
+    type: 'enum',
+    enum: RequestStatus,
+    default: RequestStatus.PENDING,
+  })
+  status!: RequestStatus;
+
+  @ManyToOne(() => Skill, { nullable: false, onDelete: 'RESTRICT' })
+  offeredSkill!: Skill;
+
+  @ManyToOne(() => Skill, { nullable: false, onDelete: 'RESTRICT' })
+  requestedSkill!: Skill;
+
+  @Column({ type: 'boolean', default: false })
+  isRead!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
