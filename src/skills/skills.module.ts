4ca45a0be6b4def1bb05c27aa3d1811ccb370836
maxererl <126725331+maxererl@users.noreply.github.com>
2026-05-22 22:40:15 +0300

    feat(skills): add skill to favorites (#122)
    * feat(skills): add skill to favorites
    
    * chore(users): updateFavoriteSkills refactor
    
    * fix(skills): update favorite skills bug and return value fixed

--- src/skills/skills.module.ts
+++ src/skills/skills.module.ts
@@ -3,11 +3,14 @@ import { SkillsService } from './skills.service';
 import { SkillsController } from './skills.controller';
 import { Skill } from './entities/skill.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { FilesService } from 'src/files/files.service';
+import { FilesService } from '../files/files.service';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Skill])],
+  imports: [TypeOrmModule.forFeature([Skill, User]), UsersModule],
   controllers: [SkillsController],
-  providers: [SkillsService, FilesService],
+  providers: [SkillsService, FilesService, UsersService],
 })
 export class SkillsModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import * as bcrypt from 'bcrypt';
 import {
+  ConflictException,
   Inject,
   Injectable,
   NotFoundException,
@@ -15,6 +16,7 @@ import { appConfig, TAppConfig } from '../config/app.config';
 import { PaginationDto } from '../common/dto/pagination.dto';
 import { PaginatedResponseDto } from '../common/dto/paginated-response.dto';
 import { FindUserDto } from './dto/find-user.dto';
+import { Skill } from '../skills/entities/skill.entity';
 
 @Injectable()
 export class UsersService {
@@ -100,6 +102,20 @@ export class UsersService {
       refreshToken: null,
     });
     return updatedUser;
+  }
+
+  async updateFavoriteSkills(userId: string, skillId: string) {
+    const user = await this.usersRepository.findOne({
+      where: { id: userId },
+      relations: ['favoriteSkills'],
+    });
+    if (!user) throw new UnauthorizedException();
+    if (user.favoriteSkills.some((skill) => skill.id === skillId))
+      throw new ConflictException('Skill is already in favorites');
+
+    user.favoriteSkills.push({ id: skillId } as Skill);
+    const updatedUser = await this.usersRepository.save(user);
+    return new FindUserDto(updatedUser);
   }
 
   async setRefreshToken(id: string, refreshToken: string | null) {
