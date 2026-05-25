d39201e67d655e8c90be66e8fb63038efd107b9d
Ekaterina <48985443+albrekhtdurer@users.noreply.github.com>
2026-05-25 16:40:33 +0300

    Создание заявки по эндпоинту POST requests (#125)
    * feat(requests): add create method

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 import { RequestsController } from './requests.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Request } from './entities/request.entity';
+import { SkillsModule } from '../skills/skills.module';
+import { SkillsService } from '../skills/skills.service';
+import { Skill } from '../skills/entities/skill.entity';
+import { FilesModule } from 'src/files/files.module';
+import { FilesService } from 'src/files/files.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Request, Skill]),
+    SkillsModule,
+    FilesModule,
+  ],
   controllers: [RequestsController],
-  providers: [RequestsService],
+  providers: [RequestsService, SkillsService, FilesService],
 })
 export class RequestsModule {}
