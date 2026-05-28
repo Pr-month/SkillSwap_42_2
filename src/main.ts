9f520f86961fbd5ce194baf97e29a1163100ce9f
Ekaterina <48985443+albrekhtdurer@users.noreply.github.com>
2026-05-28 21:50:46 +0300

    Подключить swagger в main.ts (#187)
    * feat(docs): added swagger
    
    * fix(app): added global prefix and changed docs route

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import { appConfig, TAppConfig } from './config/app.config';
 import cookieParser from 'cookie-parser';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AllExceptionFilter } from './common/all-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+  app.setGlobalPrefix('api');
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
@@ -23,6 +25,15 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Skillswap')
+    .setDescription('Skillswap API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, documentFactory, { useGlobalPrefix: true });
 
   const { port } = app.get<TAppConfig>(appConfig.KEY);
   await app.listen(port);
