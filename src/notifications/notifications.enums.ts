8326bb3ec663c04e4916050b633f99a68b4c45a8
Ekaterina Novikova <fearnocait@gmail.com>
2026-05-29 22:09:15 +0300

    feat(notifications): add logic

---
+++ src/notifications/notifications.enums.ts
@@ -0,0 +1,5 @@
+export enum NotificationType {
+  NEW_REQUEST = 'New Request',
+  REQUEST_ACCEPTED = 'Request accepted',
+  REQUEST_REJECTED = 'Request rejected',
+}
--- src/notifications/notifications.gateway.ts
+++ src/notifications/notifications.gateway.ts
@@ -1,8 +1,18 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-import { TAuthSocket } from './notifications.types';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { TAuthSocket, TNotificationPayload } from './notifications.types';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 @WebSocketGateway()
 export class NotificationsGateway {
+  @WebSocketServer()
+  server: Server;
+
   @SubscribeMessage('connection')
   async handleConnection(client: TAuthSocket) {
     const userId = client.user.sub;
@@ -12,5 +22,9 @@ export class NotificationsGateway {
     await client.join(userId);
   }
 
-  async notifyUser()
+  notifyUser(id: string, payload: TNotificationPayload) {
+    this.server
+      .to(id)
+      .emit(payload.notificationType, payload.notificationMessage);
+  }
 }
