b82da853d053834f55ca9a6ddbaf223665728caf
Barbara Step <b.barbarastep@gmail.com>
2026-06-01 21:28:29 +0300

    refactor: remove redundant access guard logic

--- src/auth/guards/jwt-access.guard.ts
+++ src/auth/guards/jwt-access.guard.ts
@@ -1,18 +1,5 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { TJwtPayload } from '../auth.types';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  handleRequest<TUser = TJwtPayload>(err: unknown, user: TUser | false): TUser {
-    if (err instanceof Error) {
-      throw err;
-    }
-
-    if (!user) {
-      throw new UnauthorizedException('Invalid access token');
-    }
-
-    return user;
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
