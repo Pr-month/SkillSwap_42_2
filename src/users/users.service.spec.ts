94cb1145959c4e34901024f4bc96205ab3a88e40
Ekaterina Novikova <fearnocait@gmail.com>
2026-06-03 20:27:14 +0300

    feat(tests): add user create test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,78 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { appConfig } from '../config/app.config';
+import { usersData } from '../seeding/data/users.data';
+import { UserRole } from './users.enums';
+import { CreateUserDto } from './dto/create-user.dto';
+import { FindUserDto } from './dto/find-user.dto';
+import { instanceToPlain } from 'class-transformer';
+import * as bcrypt from 'bcrypt';
+
+jest.mock('bcrypt');
 
 describe('UsersService', () => {
   let service: UsersService;
+  const mockUsersRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    findOneBy: jest.fn(),
+  };
+  const mockConfig = {
+    hashSalt: 10,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getRepositoryToken(User), useValue: mockUsersRepository },
+        { provide: appConfig.KEY, useValue: mockConfig },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should correctly create user and return new FindUserDto from data', async () => {
+    jest.mocked(bcrypt.hash).mockResolvedValue('hashed_password' as never);
+    const currentDate = new Date();
+    const createUserData = {
+      ...usersData[0],
+      wantToLearn: [],
+      skills: [],
+      favoriteSkills: [],
+      role: UserRole.USER,
+      password: 'hashed_password',
+    };
+    const createUserResult = {
+      ...createUserData,
+      createdAt: currentDate,
+      updatedAt: currentDate,
+      id: 'newuser-id',
+      password: 'hashed_password',
+      avatar: '',
+    };
+    const resultUser = new FindUserDto(createUserResult);
+    mockUsersRepository.create.mockReturnValue(createUserResult);
+    mockUsersRepository.save.mockResolvedValue(createUserResult);
+    const result = await service.create({
+      ...usersData[0],
+      wantToLearn: [],
+    } as CreateUserDto);
+    expect(mockUsersRepository.create).toHaveBeenCalledWith(createUserData);
+    expect(mockUsersRepository.save).toHaveBeenCalledWith(createUserResult);
+    expect(result).toStrictEqual(resultUser);
   });
 });
