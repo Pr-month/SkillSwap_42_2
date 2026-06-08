0a3d022c2c677d12e4cbf75d08cde248908ec151
Barbara Step <193152528+barbarastep@users.noreply.github.com>
2026-06-08 21:50:23 +0300

    feat: add swagger docs for cities (#220)

--- src/cities/dto/create-city.dto.ts
+++ src/cities/dto/create-city.dto.ts
@@ -1,8 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateCityDto {
+  @ApiProperty({
+    example: 'Moscow',
+    description: 'City name',
+    minLength: 1,
+    maxLength: 100,
+  })
   @IsString()
   @MinLength(1)
   @MaxLength(100)
-  name: string;
+  name!: string;
 }
